ac3c7b9fcd15b21a05208b998f39939cba0841ae
antalmate12 <30597152+antalmate12@users.noreply.github.com>
2023-02-08 18:31:17 +0100

    lil' login css

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,43 +1,71 @@
-import React, {useState} from "react";
-import useAuth from "@/hooks/useAuth";
-import {Button, Container, TextField} from "@mui/material";
+import React, { useState } from 'react'
+import useAuth from '@/hooks/useAuth'
+import { Button, TextField } from '@mui/material'
+import styles from './Login.module.scss'
+import Head from 'next/head'
 
 const LoginPage = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(null);
-  const {handleLogin} = useAuth();
+  const [highlight, setHighlight] = useState(false)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState(null)
+  const { handleLogin } = useAuth()
 
   const handleSubmit = async (e: any) => {
-    e.preventDefault();
-    setError(null);
+    e.preventDefault()
+    setError(null)
 
     try {
       const res = await handleLogin(
-        "administrator@debugentity.hu",
-        "020e107c5c8f5d88b0223f64a258ddc62a593a31"
-      );
-      console.log("LOGGED IN: ", res);
+        'administrator@debugentity.hu',
+        '020e107c5c8f5d88b0223f64a258ddc62a593a31'
+      )
+      console.log('LOGGED IN: ', res)
     } catch (err: any) {
-      setError(err?.message || "Something went wrong.");
+      setError(err?.message || 'Something went wrong.')
     }
-  };
+  }
 
   return (
-    <Container>
-      <form onSubmit={handleSubmit}>
-        {error && <pre>{JSON.stringify(error)}</pre>}
+    <>
+      <Head>
+        <title>New-Admin Login</title>
+      </Head>
 
-        <TextField type={"email"} value={username} onChange={(e) => setUsername(e.target.value)} id="email"
-                   label="E-mail" variant="filled"/>
+      <div className={styles.LoginParent}>
+        <form onSubmit={handleSubmit}>
+          {error && <pre>{JSON.stringify(error)}</pre>}
 
-        <TextField type={"password"} value={password} onChange={(e) => setPassword(e.target.value)} id="password"
-                   label="Password" variant="filled"/>
+          <TextField
+            type={'email'}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            id="email"
+            label="E-mail"
+            variant="filled"
+          />
 
-        <Button type="submit" variant={"contained"}>Login</Button>
-      </form>
-    </Container>
-  );
-};
+          <TextField
+            type={'password'}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            id="password"
+            label="Password"
+            variant="filled"
+          />
 
-export default LoginPage;
+          <Button
+            onMouseEnter={() => setHighlight(true)}
+            onMouseLeave={() => setHighlight(false)}
+            type="submit"
+            variant={highlight ? 'contained' : 'outlined'}
+          >
+            Login
+          </Button>
+        </form>
+      </div>
+    </>
+  )
+}
+
+export default LoginPage
